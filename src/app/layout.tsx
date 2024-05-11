ff17a0a8a1f385aa875bad2cf988dc2e2ee7c379
odhio <odai@intersect.inc>
2024-05-11 22:12:22 +0900

    feat: change routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./_providers";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
